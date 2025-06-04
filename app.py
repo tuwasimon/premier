d14d91376f21af86a7a3650d9e1046f08a4bcd92
tuwasimon <tuwa1076@gmail.com>
2025-06-04 15:13:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def chat():
                 "Content-Type": "application/json"
             },
             json={
-                "model": "mixtral-8x7b-32768",
+                "model": "llama3-70b-8192",
                 "messages": [
                     {
                         "role": "system",
